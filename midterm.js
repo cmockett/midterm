f3a46b6b33b12ff10447c3c4d894bae42c03c0dd
Student <student@RU-109.local>
2015-09-28 17:46:57 -0600

    2nd

--- midterm.js
+++ midterm.js
@@ -53,18 +53,159 @@ var musicApp = function($scope){
 		$scope.notesArray.push("G#/Ab")
 		console.log($scope.notesArray)
 	}
+
+
+	$scope.keysArray = [
+	{
+		key: "A",
+	},
+	{
+		key: "A#/Bb",
+	},
+	{
+		key: "B",
+	},
+	{
+		key: "C",
+	},
+	{
+		key: "C#/Db",
+	},
+	{
+		key: "D",
+	},
+	{
+		key: "D#/Eb",
+	},
+	{
+		key: "E",
+	},
+	{
+		key: "F",
+	},
+	{
+		key: "F#/Gb",
+	},
+	{
+		key: "G",
+	},
+	{
+		key: "G#/Ab",
+	},
+	{
+		key: "A",
+	},
+	{
+		key: "A#/Bb",
+	},
+	{
+		key: "B",
+	},
+	{
+		key: "C",
+	},
+	{
+		key: "C#/Db",
+	},
+	{
+		key: "D",
+	},
+	{
+		key: "D#/Eb",
+	},
+	{
+		key: "E",
+	},
+	{
+		key: "F",
+	},
+	{
+		key: "F#/Gb",
+	},
+	{
+		key: "G",
+	},
+	{
+		key: "G#/Ab",
+	},
+]
+
+	$scope.scalesArray = [
+	{
+		scale: "Ionian (Major)",
+		scalePattern: [2,2,1,2,2,2,1]
+	},
+	{
+		scale: "Dorian",
+		scalePattern: [2,1,2,2,2,1,2]
+	},
+	{
+		scale: "Phrygian",
+		scalePattern: [1,2,2,2,1,2,2]
+	},
+	{
+		scale: "Lydian",
+		scalePattern: [2,2,2,1,2,2,1]
+	},
+	{
+		scale: "Mixolydian",
+		scalePattern: [2,2,1,2,2,1,2]
+	},
+	{
+		scale: "Aeolian (Minor)",
+		scalePattern: [2,1,2,2,1,2,2]
+	},
+	{
+		scale: "Locrian",
+		scalePattern: [1,2,2,1,2,2,2]
+	},
+	{
+		scale: "Half-Whole Diminished",
+		scalePattern: [1,2,1,2,1,2,1,2]
+	},
+	{
+		scale: "Whole-Half Diminished",
+		scalePattern: [2,1,2,1,2,1,2,1]
+	},
+	{
+		scale: "Harmonic Minor",
+		scalePattern: [2,1,2,2,1,3,1]
+	},
+	{
+		scale: "Melodic Minor",
+		scalePattern: [2,1,2,2,2,2,1]
+	}
+	]
+
+	
+	$scope.setKeyScale = function(){
+		$scope.compareArray.push($scope.selectKeys)
+				// for(var i =0; i < $scope.selectScale.length; i++){
+		// 	for(var j=$scope.selectKeys; j < $scope.keysArray.length; j+[i]){
+		// 		$scope.compareArray.push(j)
+		// 	}
+		// }
+			for(var i = 0; i<$scope.selectScale.length; i+=$scope.selectScale[i]){
+				$scope.compareArray.push()
+			}
+
+
+
+
+
+		console.log($scope.selectKeys)
+		console.log($scope.selectScale)
+		console.log($scope.compareArray)
+	}
+
 	$scope.submitButtonClick = function(){
 		if($scope.notesArray.join() === $scope.compareArray.join()){
-			alert("You win!")
+			alert("Correct!")
 		}
 		else{
 			alert("Try Again!")
 		}
 	}
-
-
-
-
 }
 
 
