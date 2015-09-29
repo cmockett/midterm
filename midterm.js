35a477816fa7bce499fbef13354bc7a945eb0037
Student <student@RU-109.local>
2015-09-29 12:00:28 -0600

    3rd

--- midterm.js
+++ midterm.js
@@ -132,74 +132,111 @@ var musicApp = function($scope){
 
 	$scope.scalesArray = [
 	{
-		scale: "Ionian (Major)",
+		scaleName: "Ionian (Major)",
 		scalePattern: [2,2,1,2,2,2,1]
 	},
 	{
-		scale: "Dorian",
+		scaleName: "Dorian",
 		scalePattern: [2,1,2,2,2,1,2]
 	},
 	{
-		scale: "Phrygian",
+		scaleName: "Phrygian",
 		scalePattern: [1,2,2,2,1,2,2]
 	},
 	{
-		scale: "Lydian",
+		scaleName: "Lydian",
 		scalePattern: [2,2,2,1,2,2,1]
 	},
 	{
-		scale: "Mixolydian",
+		scaleName: "Mixolydian",
 		scalePattern: [2,2,1,2,2,1,2]
 	},
 	{
-		scale: "Aeolian (Minor)",
+		scaleName: "Aeolian (Minor)",
 		scalePattern: [2,1,2,2,1,2,2]
 	},
 	{
-		scale: "Locrian",
+		scaleName: "Locrian",
 		scalePattern: [1,2,2,1,2,2,2]
 	},
 	{
-		scale: "Half-Whole Diminished",
+		scaleName: "Half-Whole Diminished",
 		scalePattern: [1,2,1,2,1,2,1,2]
 	},
 	{
-		scale: "Whole-Half Diminished",
+		scaleName: "Whole-Half Diminished",
 		scalePattern: [2,1,2,1,2,1,2,1]
 	},
 	{
-		scale: "Harmonic Minor",
+		scaleName: "Harmonic Minor",
 		scalePattern: [2,1,2,2,1,3,1]
 	},
 	{
-		scale: "Melodic Minor",
+		scaleName: "Melodic Minor",
 		scalePattern: [2,1,2,2,2,2,1]
 	}
 	]
 
 	
 	$scope.setKeyScale = function(){
-		$scope.compareArray.push($scope.selectKeys)
+		// $scope.compareArray.push($scope.selectKeys)
 				// for(var i =0; i < $scope.selectScale.length; i++){
 		// 	for(var j=$scope.selectKeys; j < $scope.keysArray.length; j+[i]){
 		// 		$scope.compareArray.push(j)
 		// 	}
 		// }
-			for(var i = 0; i<$scope.selectScale.length; i+=$scope.selectScale[i]){
-				$scope.compareArray.push()
+			// for(var i = 0; i<$scope.selectScale.length; i+=$scope.selectScale[i]){
+			// }
+			
+			$scope.keyIndex
+			for(var i=0; i<($scope.keysArray.length/2); i++){
+				if($scope.keysArray[i].key === $scope.selectKeys){
+					$scope.keyIndex = i
+				}
+			}
+			// for(var j = $scope.keyIndex; j<$scope.keysArray.length; j+= $scope.selectScale[j]){
+			// 		console.log("hi!")
+			// 		$scope.compareArray.push($scope.keysArray[j].key)
+
+			// }
+
+
+			
+			var position = $scope.keyIndex
+			for ( var i = 0; i < $scope.scalesArray[$scope.selectScale].scalePattern.length; i++ ) {
+				$scope.compareArray.push($scope.keysArray[position])
+				position += $scope.scalesArray[$scope.selectScale].scalePattern[i]
+
 			}
 
+			// function printObject(o) {
+			// 	  var out = '';
+			// 	  for (var p in o) {
+			// 	    out += o[p] 
+			// 	  }
+			// 	  return out;
+			// 	}
 
+			// var notes = $scope.compareArray.filter(function printObject(o) {
+			// 	  var out = '';
+			// 	  for (var p in o) {
+			// 	    out += o[p] 
+			// 	  }
+			// 	  return out;
+			// 	})
+			
+			 $scope.compareArray.filter(function(el){
+				notes.push(el.key);
+			})
 
-
-
-		console.log($scope.selectKeys)
-		console.log($scope.selectScale)
-		console.log($scope.compareArray)
+			// console.log($scope.compareArray)
+			console.log(notes)
+		// console.log($scope.selectKeys)
+		// console.log($scope.compareArray)
 	}
-
+	var notes = []
 	$scope.submitButtonClick = function(){
-		if($scope.notesArray.join() === $scope.compareArray.join()){
+		if($scope.notesArray.join() === notes.join()){
 			alert("Correct!")
 		}
 		else{
